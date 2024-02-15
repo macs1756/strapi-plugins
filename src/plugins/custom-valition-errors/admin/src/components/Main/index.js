6b78eff50a9db2d62212b7e50b291bcb1acfed02
macs1756 <kindrativmaksum312@gmail.com>
2024-02-15 17:11:34 +0200

    v17:11

--- src/plugins/custom-valition-errors/admin/src/components/Main/index.js
+++ src/plugins/custom-valition-errors/admin/src/components/Main/index.js
@@ -1,7 +1,7 @@
 // @ts-nocheck
 import { useCMEditViewDataManager } from "@strapi/helper-plugin";
 import { useEffect, useState } from "react";
-import { Alert, Box, Typography } from '@strapi/design-system';
+import { Alert, Box, GridLayout, Typography } from '@strapi/design-system';
 
 function Main() {
   const [errorsFields, setErrorsFields] = useState({})
@@ -12,15 +12,31 @@ function Main() {
   useEffect(() => {
     if (Object.keys(formErrors).length !== 0) {
       setIsOpen(true)
-      setErrorsFields(Object.keys(formErrors).toString())
+      setErrorsFields(Object.keys(formErrors));
     }
 
   }, [formErrors])
 
   return (
-    isOpen && <Box style={{ width: 600, position: 'fixed', top: '15%', left: '50%', transform: 'translateX(-35%)' }}>
-      <Alert title="To proceed, resolve the validation errors." variant="danger" onClose={()=>{setIsOpen(false)}}><br/>
-        <Box style={{display: 'block', width: '100%'}}>{errorsFields}</Box>
+    isOpen && <Box style={{ width: 600, position: 'absolute', top: '15%', left: '50%', transform: 'translateX(-35%)' }}>
+      <Alert title="To proceed, resolve the validation errors." variant="danger" onClose={() => { setIsOpen(false) }}><br />
+        <GridLayout style={{}}>
+
+          {
+            errorsFields.map((e, i) => {
+
+              if (/\.\w+\./.test(e)) {
+                const part = e.split('.')
+                return <Typography key={'errors' + e + i}>{`A validation error was found in the component №${+part[1] + 1} ${part[0]}, field ${part[2]}`}</Typography>
+
+              } else {
+                return <Typography key={'errors' + e + i}>{`A validation error was found in the field ${e}`}</Typography>
+              }
+            })
+          }
+        </GridLayout>
+
+
       </Alert>
     </Box>
 
