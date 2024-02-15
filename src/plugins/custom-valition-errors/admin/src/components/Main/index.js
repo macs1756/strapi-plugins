107096e47bf38bc6508487717eda17a254a0803a
macs1756 <kindrativmaksum312@gmail.com>
2024-02-15 16:15:38 +0200

    v16:15

--- src/plugins/custom-valition-errors/admin/src/components/Main/index.js
+++ src/plugins/custom-valition-errors/admin/src/components/Main/index.js
@@ -1,25 +1,29 @@
 // @ts-nocheck
 import { useCMEditViewDataManager } from "@strapi/helper-plugin";
 import { useEffect, useState } from "react";
-import { Alert } from '@strapi/design-system';
+import { Alert, Box, Typography } from '@strapi/design-system';
 
 function Main() {
-  const [errorsFields, setErrorsFields] = useState()
+  const [errorsFields, setErrorsFields] = useState({})
+  const [isOpen, setIsOpen] = useState(false)
 
   const { modifiedData, layout, onChange, updateActionAllowedFields, onPublish, onPublishPromptDismissal, onUnpublish, formErrors } = useCMEditViewDataManager();
 
   useEffect(() => {
     if (Object.keys(formErrors).length !== 0) {
-      setErrorsFields(JSON.stringify(formErrors).toString())
+      setIsOpen(true)
+      setErrorsFields(Object.keys(formErrors).toString())
     }
 
   }, [formErrors])
 
   return (
-    errorsFields &&
-    <Alert closeLabel="Close" title="This is the title of the alert" action=''>
-      {errorsFields}
-    </Alert>
+    isOpen && <Box style={{ width: 600, position: 'fixed', top: '15%', left: '50%', transform: 'translateX(-35%)' }}>
+      <Alert title="To proceed, resolve the validation errors." variant="danger" onClose={()=>{setIsOpen(false)}}><br/>
+        <Box style={{display: 'block', width: '100%'}}>{errorsFields}</Box>
+      </Alert>
+    </Box>
+
   );
 }
 
